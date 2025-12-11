f4824f9d596b65e3c855c9f324fa6f2d9626b81d
Victor Alann <victorAlannr@gmail.com>
2025-12-11 06:02:36 +0000

    aumente so mais um pouco a saturação do preto e deixe so mais um pouco r

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -112,7 +112,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'gradient-flow': 'gradient-flow 6s linear infinite',
+        'gradient-flow': 'gradient-flow 4s linear infinite',
         'float': 'float 5s ease-in-out infinite',
       },
     },
