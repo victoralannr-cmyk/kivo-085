e2a581bd439f46dcfaab1a0367c64434501c15e9
Victor Alann <victorAlannr@gmail.com>
2025-12-11 05:08:04 +0000

    a logo que esta no lado esquerdo de onde esta escrito © 2025 Kivo. Todos

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
     <footer className="border-t border-border/40 py-6">
-      <div className="container flex items-center justify-center gap-2">
+      <div className="container flex flex-col items-center justify-center gap-2">
         <Image 
           src="https://i.postimg.cc/QxKrKJ9z/Captura-de-tela-2025-11-30-224857-removebg-preview.png" 
           alt="Kivo Logo"
