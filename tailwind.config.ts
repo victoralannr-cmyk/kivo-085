10fdfa01d84770b63fd9f295e60105517c9a2a15
Victor Alann <victorAlannr@gmail.com>
2025-12-11 04:10:14 +0000

    para ser mais claro eu quero que o efeito do botão onde esta escrito age

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -108,7 +108,7 @@ export default {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         'gradient-flow': 'gradient-flow 6s ease infinite',
-        'pulse-glow': 'pulse-glow 10s ease-in-out infinite',
+        'pulse-glow': 'pulse-glow 20s ease-in-out infinite',
       },
     },
   },
