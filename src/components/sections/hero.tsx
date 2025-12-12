aa2f34441e491780aee1536d70e46ee6b392aa29
Victor Alann <victorAlannr@gmail.com>
2025-12-12 22:38:02 +0000

    puta que pariu as palavras não estão aparecendo do mesmo jeito, eu não q

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -1,7 +1,6 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { WhatsAppIcon } from "@/components/ui/icons";
-import CascadingLetters from "../ui/cascading-letters";
 
 const HeroSection = () => {
   return (
@@ -11,12 +10,7 @@ const HeroSection = () => {
       <div className="absolute inset-0 -z-10 h-full w-full bg-gradient-to-b from-transparent via-transparent to-background"></div>
       <div className="container relative z-10 text-center">
         <h1 className="font-headline text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
-          <CascadingLetters
-            text="Impulsionamos sua empresa para o topo com modelos preditivos de conversão"
-            highlightedWords={['Impulsionamos', 'para', 'preditivos', 'de', 'conversão']}
-            highlightClassName="text-wavy-gradient"
-            stagger={0.02}
-          />
+          Impulsionamos sua empresa para o topo com modelos preditivos de conversão
         </h1>
         <p className="mx-auto mt-6 max-w-2xl text-lg text-foreground/80">
           e mantemos você dominante, relevante e sempre{' '}
