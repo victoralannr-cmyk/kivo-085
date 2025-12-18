61a0ac1aff33fb9eea94b9c305d4fe37af9c6763
Victor Alann <victorAlannr@gmail.com>
2025-12-18 19:59:52 +0000

    Type error: 'codeEl' is possibly 'null'.

--- src/components/ui/animated-code-block.tsx
+++ src/components/ui/animated-code-block.tsx
@@ -10,7 +10,10 @@ const AnimatedCodeBlock = () => {
   useEffect(() => {
     const observer = new IntersectionObserver(
       ([entry]) => {
-        setIsIntersecting(entry.isIntersecting);
+        if (entry.isIntersecting) {
+          setIsIntersecting(true);
+          observer.unobserve(entry.target);
+        }
       },
       { threshold: 0.1 }
     );
