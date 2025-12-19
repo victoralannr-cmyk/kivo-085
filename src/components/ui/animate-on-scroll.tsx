3ba635088d38ab4f16fdab09cc69380616b605f6
Victor Alann <victorAlannr@gmail.com>
2025-12-19 04:50:09 +0000

    a animação esta muito dura e robotizada, deixe ela mais suave

--- src/components/ui/animate-on-scroll.tsx
+++ src/components/ui/animate-on-scroll.tsx
@@ -60,7 +60,7 @@ const AnimateOnScroll = ({
   return (
     <div
       ref={ref}
-      className={cn(className, 'transition-opacity duration-500', isVisible ? `${animationClassName} opacity-100` : 'opacity-0')}
+      className={cn(className, 'transition-opacity duration-1000', isVisible ? `${animationClassName} opacity-100` : 'opacity-0')}
       style={{ ...animationStyle, ...props.style }}
       {...props}
     >
