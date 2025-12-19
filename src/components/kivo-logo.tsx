f7a1e0936cf3208ae460c15113bbd72aac1a91c4
Victor Alann <victorAlannr@gmail.com>
2025-12-19 05:55:37 +0000

    a alteração deve ser feita nessa favicon que esta sobrepondo a que eu qu

--- src/components/kivo-logo.tsx
+++ src/components/kivo-logo.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 const KivoLogo = ({ className }: { className?: string }) => {
   return (
     <Image
-      src="https://i.postimg.cc/K8MgDrs5/Captura-de-tela-2025-11-27-221300-removebg-preview.png"
+      src="https://i.imgur.com/NWwb87f.png"
       alt="Kivo Logo"
       width={168}
       height={48}
